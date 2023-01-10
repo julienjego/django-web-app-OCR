956933d169a4a991bd9a2b9e36670842d93bf039
julienjego <jego.jj@gmail.com>
2023-01-10 09:23:12 +0100

    Add contact form and refactor nav

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.mail import send_mail
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm
 
 
 def band_list(request):
@@ -29,4 +31,28 @@ def item_detail(request, id):
 
 
 def contact(request):
-    return render(request, "listings/contact.html")
+    if request.method == "POST":
+        # créer une instance de notre formulaire et le remplir
+        # avec les données POST
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f"""Message from {form.cleaned_data["name"]
+                        or "anonyme"} via MerchEx Contact Us form""",
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data["email"],
+                recipient_list=["admin@merchex.xyz"],
+            )
+            return redirect("email-sent")
+    # si le formulaire n'est pas valide, nous laissons l'exécution
+    # continuer jusqu'au return
+    # ci-dessous et afficher à nouveau le formulaire (avec des erreurs).
+    else:
+        form = ContactUsForm()
+
+    return render(request, "listings/contact.html", {"form": form})
+
+
+def contact_success(request):
+    return render(request, "listings/contact_success.html")
